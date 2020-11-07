7c26a5f1d099ca0dab1d1e95013574d88214a6d7
Nikolai Limbrunner <nikolai.limbrunner@motius.de>
2020-11-07 22:19:45 +0100

    Included images in pip package

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup 
 
-setup(name='gym_cabworld', version='0.1', install_requires=['gym', 'pygame'])
+setup(name='gym_cabworld', 
+      version='0.2', 
+      install_requires=['gym', 'pygame'],
+      author='Nikolai Limbrunner', 
+      author_email='nikolai.limbrunner@web.de',
+      packages=['gym_cabworld'], 
+      package_dir={'gym_cabworld':'gym_cabworld'},
+      package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        'gym_cabworld': ['images/*.png'],
+    },)
 
