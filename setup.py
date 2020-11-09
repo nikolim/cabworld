6eb0052087ffb1264bc5a347d4ee23b841adbc09
Nikolai Limbrunner <nikolai.limbrunner@motius.de>
2020-11-09 23:07:21 +0100

    WIP:Distro, relative imports dont work yet

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup 
+import os
+from setuptools import setup, find_packages
 
-setup(name='gym_cabworld', 
-      version='0.2', 
+setup(name='gym_cabworld',
+      version='0.2',
       install_requires=['gym', 'pygame'],
-      author='Nikolai Limbrunner', 
+      author='Nikolai Limbrunner',
       author_email='nikolai.limbrunner@web.de',
-      packages=['gym_cabworld'], 
-      package_dir={'gym_cabworld':'gym_cabworld'},
-      package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        'gym_cabworld': ['images/*.png'],
-    },)
-
+      packages=find_packages(),
+      package_dir={'gym_cabworld': 'gym_cabworld'},
+      package_data={
+          '': ['*.png'], },
+      data_files=[
+        ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'map_gen.png')]),
+        ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'cab.png')]),
+        ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'person_1.png')]),
+        ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'person_2.png')]),
+        ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'person_3.png')]),
+    ],
+)
