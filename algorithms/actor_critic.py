fac9d05ef87e782be70176ef7bab5e0c6c4becf1
Nikolai Limbrunner <nikolai.limbrunner@motius.de>
2020-11-25 11:21:56 +0100

    Added actor-critic template from rl-cookbook

--- algorithms/actor_critic.py
+++ algorithms/actor_critic.py
@@ -1,183 +1,179 @@
-import argparse
-import gym
-import gym_cabworld 
-import numpy as np
-from itertools import count
-from collections import namedtuple
-
+import os
+import time
 import torch
+import gym
+import gym_cabworld
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.distributions import Categorical
+from torch.utils.tensorboard import SummaryWriter
 
-parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                    help='discount factor (default: 0.99)')
-parser.add_argument('--seed', type=int, default=543, metavar='N',
-                    help='random seed (default: 543)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='interval between training status logs (default: 10)')
-args = parser.parse_args()
+from pyvirtualdisplay import Display
+disp = Display().start()
+
+# Create a new log folder for tensorboard
+log_folders = os.listdir('../runs')
+if len(log_folders) == 0:
+    folder_number = 0
+else:
+    folder_number = max([int(elem) for elem in log_folders]) + 1
+log_path = os.path.join("../runs", str(folder_number))
+writer = SummaryWriter(log_path)
 
 env = gym.make('Cabworld-v0')
-env.seed(args.seed)
-torch.manual_seed(args.seed)
 
-SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
-
-class Policy(nn.Module):
+def track_reward(reward, saved_rewards):
     """
-    implements both actor and critic in one model
+    Count the number of rewards / penalties
+    @param reward: reward for last action 
+    @param saved_rewards: tupel of previous received rewards
     """
-    def __init__(self):
-        super(Policy, self).__init__()
-        # create features from input
-        self.affine1 = nn.Linear(12, 64)
+    saved_rewards = list(saved_rewards)
+    if reward == -10:
+        saved_rewards[0] += 1
+    if reward == -110:
+        saved_rewards[1] += 1
+    if reward == -510:
+        saved_rewards[2] += 1
+    return tuple(saved_rewards)
 
-        # actor's layer
-        self.action_in = nn.Linear(64, 128)
-        self.action_hidden = nn.Linear(128, 128)
-        self.action_out = nn.Linear(128, 6)
+def log_rewards(writer, saved_rewards, episode_reward, episode): 
+    """
+    Log rewards for tensorboard
+    @param writer: writer to write to into logs
+    @param saved_rewards: Tupel with penalties (path, pick-up, illegal-move)
+    @param episode_reward: reward for the current episode
+    @param episode: curent number of episode
+    """
+    writer.add_scalar('Path Penalty', saved_rewards[0], episode)
+    writer.add_scalar('Illegal Pick-up / Drop-off', saved_rewards[1], episode)
+    writer.add_scalar('Illegal Move', saved_rewards[2], episode)
+    writer.add_scalar('Reward', episode_reward, episode)
 
-        # critic's layer
-        self.value_in = nn.Linear(64, 128)
-        self.value_hidden = nn.Linear(128, 128)
-        self.value_out = nn.Linear(128, 1)
-
-        # action & reward buffer
-        self.saved_actions = []
-        self.rewards = []
+class ActorCriticModel(nn.Module):
+    def __init__(self, n_input, n_output, n_hidden):
+        super(ActorCriticModel, self).__init__()
+        self.fc1 = nn.Linear(n_input, n_hidden[0])
+        self.fc2 = nn.Linear(n_hidden[0], n_hidden[1])
+        self.action = nn.Linear(n_hidden[1], n_output)
+        self.value = nn.Linear(n_hidden[1], 1)
 
     def forward(self, x):
+        x = torch.Tensor(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        action_probs = F.softmax(self.action(x), dim=-1)
+        state_values = self.value(x)
+        return action_probs, state_values
+
+class PolicyNetwork():
+    def __init__(self, n_state, n_action, n_hidden, lr=0.001):
+
+        self.model = ActorCriticModel(n_state, n_action, n_hidden)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=10, gamma=0.9)
+
+    def predict(self, s):
         """
-        forward of both actor and critic
+        Compute the output using the Actor Critic model
+        @param s: input state
+        @return: action probabilities, state_value
         """
-        x = F.relu(self.affine1(x))
-
-        # actor: choses action to take from state s_t 
-        # by returning probability of each action
-        actor_x = F.relu(self.action_in(x))
-        actor_x = F.relu(self.action_hidden(actor_x))
-        action_prob = F.softmax(self.action_out(actor_x), dim=-1)
-
-        # critic: evaluates being in the state s_t
-        state_x = F.relu(self.value_in(x))
-        state_x = F.relu(self.value_hidden(state_x))
-        state_values = self.value_out(state_x)
-
-        # return values for both actor and critic as a tuple of 2 values:
-        # 1. a list with the probability of each action over the action space
-        # 2. the value from state s_t 
-        return action_prob, state_values
+        return self.model(torch.Tensor(s))
 
 
-model = Policy()
-optimizer = optim.Adam(model.parameters(), lr=3e-2)
-eps = np.finfo(np.float32).eps.item()
+    def update(self, returns, log_probs, state_values):
+        """
+        Update the weights of the Actor Critic network given the training samples
+        @param returns: return (cumulative rewards) for each step in an episode
+        @param log_probs: log probability for each step
+        @param state_values: state-value for each step
+        """
+        loss = 0
+        for log_prob, value, Gt in zip(log_probs, state_values, returns):
+            advantage = Gt - value.item()
+            policy_loss = -log_prob * advantage
+
+            value_loss = F.smooth_l1_loss(value, Gt)
+
+            loss += policy_loss + value_loss
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
 
-def select_action(state):
-    state = torch.from_numpy(state).float()
-    probs, state_value = model(state)
-
-    # create a categorical distribution over the list of probabilities of actions
-    m = Categorical(probs)
-
-    # and sample an action using the distribution
-    action = m.sample()
-
-    # save to action buffer
-    model.saved_actions.append(SavedAction(m.log_prob(action), state_value))
-
-    # the action to take (left or right)
-    return action.item()
+    def get_action(self, s):
+        """
+        Estimate the policy and sample an action, compute its log probability
+        @param s: input state
+        @return: the selected action, log probability, predicted state-value
+        """
+        action_probs, state_value = self.predict(s)
+        action = torch.multinomial(action_probs, 1).item()
+        log_prob = torch.log(action_probs[action])
+        return action, log_prob, state_value
 
 
-def finish_episode():
+def actor_critic(env, estimator, n_episode, gamma=1.0):
     """
-    Training code. Calculates actor and critic loss and performs backprop.
+    Actor Critic algorithm
+    @param env: Gym environment
+    @param estimator: policy network
+    @param n_episode: number of episodes
+    @param gamma: the discount factor
     """
-    R = 0
-    saved_actions = model.saved_actions
-    policy_losses = [] # list to save actor (policy) loss
-    value_losses = [] # list to save critic (value) loss
-    returns = [] # list to save the true values
-
-    # calculate the true value using rewards returned from the environment
-    for r in model.rewards[::-1]:
-        # calculate the discounted value
-        R = r + args.gamma * R
-        returns.insert(0, R)
-
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-
-    for (log_prob, value), R in zip(saved_actions, returns):
-        advantage = R - value.item()
-
-        # calculate actor (policy) loss 
-        policy_losses.append(-log_prob * advantage)
-
-        # calculate critic (value) loss using L1 smooth loss
-        value_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
-
-    # reset gradients
-    optimizer.zero_grad()
-
-    # sum up all the values of policy_losses and value_losses
-    loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
-
-    # perform backprop
-    loss.backward()
-    optimizer.step()
-
-    # reset rewards and action buffer
-    del model.rewards[:]
-    del model.saved_actions[:]
-
-
-def main():
-    running_reward = 10
-
-    # run inifinitely many episodes
-    for i_episode in range(1000):
-
-        # reset environment and episode reward
+    for episode in range(n_episode):
+        log_probs = []
+        rewards = []
+        state_values = []
         state = env.reset()
-        ep_reward = 0
-
-        # for each episode, only run 9999 steps so that we don't 
-        # infinite loop while learning
+        saved_rewards = (0,0,0)
         while True:
+            action, log_prob, state_value = estimator.get_action(state)
+            next_state, reward, is_done, _ = env.step(action)
+            saved_rewards = track_reward(reward, saved_rewards)
+            total_reward_episode[episode] += reward
+            log_probs.append(log_prob)
+            state_values.append(state_value)
+            rewards.append(reward)
 
-            # select action from policy
-            action = select_action(np.array(state))
+            if is_done:
 
-            # take the action
-            state, reward, done, _ = env.step(action)
+                returns = []
+                Gt = 0
+                pw = 0
 
-            if args.render:
-                env.render()
+                for reward in rewards[::-1]:
+                    Gt += gamma ** pw * reward
+                    pw += 1
+                    returns.append(Gt)
 
-            model.rewards.append(reward)
-            ep_reward += reward
-            if done:
+                returns = returns[::-1]
+                returns = torch.tensor(returns)
+                returns = (returns - returns.mean()) / (returns.std() + 1e-9)
+                estimator.update(returns, log_probs, state_values)
+                print('Episode: {}, total reward: {}'.format(episode, total_reward_episode[episode]))
+                log_rewards(writer, saved_rewards, total_reward_episode[episode], episode)
+                if total_reward_episode[episode] >= -14:
+                    estimator.scheduler.step()
                 break
+            state = next_state
 
-        # update cumulative reward
-        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+n_state = 12
+n_action = env.action_space.n
+n_hidden = [128, 32]
+lr = 0.03
+policy_net = PolicyNetwork(n_state, n_action, n_hidden, lr)
 
-        # perform backprop
-        finish_episode()
+n_episode = 100
+gamma = 0.9
+total_reward_episode = [0] * n_episode
 
-        # log results
-        if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                  i_episode, ep_reward, running_reward))
+actor_critic(env, policy_net, n_episode, gamma)
 
-
-if __name__ == '__main__':
-    main()
+import matplotlib.pyplot as plt
+plt.plot(range(100, n_episode), total_reward_episode[100:])
+plt.title('Episode reward over time')
+plt.xlabel('Episode')
+plt.ylabel('Total reward')
+plt.show()
