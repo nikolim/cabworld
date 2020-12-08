93150b075d7dff7246ca5d5256493d0871a456ac
Nikolai Limbrunner <nikolai.limbrunner@motius.de>
2020-12-08 16:56:47 +0100

    Updated setup and requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='gym_cabworld',
-      version='0.2',
-      install_requires=['gym', 'pygame'],
+      version='0.9',
+      install_requires=['gym', 'pygame', 'wheel'],
       author='Nikolai Limbrunner',
       author_email='nikolai.limbrunner@web.de',
       packages=find_packages(),
@@ -12,10 +12,12 @@ setup(name='gym_cabworld',
           '': ['*.png'], },
 	data_files=[
         ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'map_gen.png')]),
+        ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'small_map_gen.png')]),
         ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'cab.png')]),
         ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'person_1.png')]),
         ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'person_2.png')]),
         ('/home/niko/Info/gym-cabworld', [os.path.join('gym_cabworld/images/', 'person_3.png')]),
         ('/home/niko/Info/gym-cabworld', [os.path.join('map.dat')]),
+         ('/home/niko/Info/gym-cabworld', [os.path.join('small_map.dat')]),
     ],
 )
