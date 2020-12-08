198d7f3e7004d3a3d30c5b8d0e27d518c84d8ca2
Nikolai Limbrunner <nikolai.limbrunner@motius.de>
2020-12-08 15:41:53 +0100

    Integrated small world

--- gym_cabworld/envs/__init__.py
+++ gym_cabworld/envs/__init__.py
@@ -1,2 +1,2 @@
-from gym_cabworld.envs.custom_env import CustomEnv, CustomEnv1, CustomEnv2, MarlEnv
+from gym_cabworld.envs.custom_env import CustomEnv, CustomEnv0, CustomEnv1, CustomEnv2, MarlEnv, CustomEnv4, CustomEnv5, CustomEnv6, MarlEnv2
 from gym_cabworld.envs.game import Game
