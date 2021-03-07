b1d17a7d874438f03d63a9aac931e5f473e594df
Nikolai Limbrunner <nikolai.limbrunner@motius.de>
2021-03-07 21:08:09 +0100

    Fixed setup for deployment on PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Farmworld Reinforcement Environment",
     long_description="Reinforcement Learning environment"
     "efficiently to their destination. Based on OpenAIGym and Pygame",
-    version="0.0.1",
+    version="0.0.2",
     install_requires=["gym", "pygame", "wheel"],
     author="Nikolai Limbrunner",
     author_email="nikolai.limbrunner@web.de",
@@ -17,26 +17,4 @@ setup(
         "gym_farmworld/images/": ["gym_farmworld/images/*.png"],
         "gym_farmworld/data/": ["gym_farmworld/data/*.dat"],
     },
-    data_files=[
-        ("/gym_farmworld/images", [os.path.join("gym_farmworld/images/", "map_gen.png")]),
-        (
-            "/gym_farmworld/images",
-            [os.path.join("gym_farmworld/images/", "small_map_gen.png")],
-        ),
-        ("/gym_farmworld/images", [os.path.join("gym_farmworld/images/", "cab.png")]),
-        (
-            "/gym_farmworld/images",
-            [os.path.join("gym_farmworld/images/", "person_1.png")],
-        ),
-        (
-            "/gym_farmworld/images",
-            [os.path.join("gym_farmworld/images/", "person_2.png")],
-        ),
-        (
-            "/gym_farmworld/images",
-            [os.path.join("gym_farmworld/images/", "person_3.png")],
-        ),
-        ("/gym_farmworld/data", [os.path.join("gym_farmworld/data/small_map.dat")]),
-        ("/gym_farmworld/data", [os.path.join("gym_farmworld/data/map.dat")]),
-    ],
 )
